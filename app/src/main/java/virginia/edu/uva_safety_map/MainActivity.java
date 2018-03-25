6157c6ee81796231d284601a6e3810ff49c69670
A. Shi <as4ac@virginia.edu>
2018-03-25 04:14:09 -0400

    End of day 1

--- app/src/main/java/virginia/edu/uva_safety_map/MainActivity.java
+++ app/src/main/java/virginia/edu/uva_safety_map/MainActivity.java
@@ -2,6 +2,7 @@ package virginia.edu.uva_safety_map;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,11 +11,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private static final LatLng UVA = new LatLng(38.0336, -78.5080);
+    private static final LatLng UVA = new LatLng(38.0336, -78.507980);
+    private FirebaseDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        db = FirebaseDatabase.getInstance();
     }
 
 
@@ -43,11 +55,53 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 //        Start the map at Charlottesville
         mMap.setMaxZoomPreference(40f);
         mMap.setMinZoomPreference(15f);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(38.0336, -78.507980)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(UVA));
 
-        Marker uva = mMap.addMarker(new MarkerOptions()
-                .position(UVA)
-                .title("University of Virginia")
-                .snippet("go hoos"));
+        mMap.setMyLocationEnabled(true);
+        displayHistory(30);
+
+//        Marker uva = mMap.addMarker(new MarkerOptions()
+//                .position(UVA)
+//                .title("University of Virginia")
+//                .snippet("go hoos"));
+
+
+    }
+
+    private void displayHistory(int d){
+        int day = 19;
+        int mon = 3;
+        for(int i = 0; i < d; i++) {
+            DatabaseReference myRef = db.getReference("2018-" + String.format("%02d", mon) + "-" + day);
+            day--;
+            if(day == 0){
+                day = 28;
+                mon--;
+            }
+
+            // TAKE LOCATION, PLACE PIN WITH POINT DESCRIPTION AND CIRCLE
+            myRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Map<String, String> td = new HashMap<String, String>();
+                    for (DataSnapshot loc : dataSnapshot.getChildren()) {
+                        td.put(loc.getKey(), loc.getValue(String.class));
+                    }
+
+//                ArrayList<Job_Class> values = new ArrayList<>(td.values());
+//                List<String> keys = new ArrayList<String>(td.keySet());
+
+                    for (String s : td.keySet()) {
+                        Log.d("firebase", s + " : " + td.get(s));
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w("DATABASE", "Failed to read value.", error.toException());
+                }
+            });
+        }
     }
 }
